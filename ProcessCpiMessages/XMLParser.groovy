3edfcf22c28c090b2230a52bb33233973fd0a797
Carlos Pereira <carlos.rodrigo@siemens.com>
2025-11-14 09:50:27 +0000

    Insert comments to execute the Maven instructions to include the local sap libraries

--- ProcessCpiMessages/XMLParser.groovy
+++ ProcessCpiMessages/XMLParser.groovy
@@ -18,32 +18,28 @@ import java.nio.file.Files
  ----------------------------------------------------------------------------------------------------------*/
 
 
-//********************************************************************
-//--- Main Class ( not copy it to CPI ---
-//  just fill the properties or change the payload that you need use to  process
-// and fill properties or headers according your process
+GroovyShell shell = new GroovyShell()
 
-static void main(String[] args) {
-    Message message
-    //def path = Paths.get("C://teste//idoc_tanker.xml")
-    def body =  new File("C:/teste/idoc_tanker.xml").text
-    CamelContext context = new DefaultCamelContext()
-    Exchange exchange = new DefaultExchange(context)
+Script script = shell.parse(new File("C:/Users/z003xkny/CPIGroovyEmulator/ProcessCpiMessages/XMLParser.groovy"))
+
+def body = new File("C:/teste/TesteXML.xml").text
+CamelContext context = new DefaultCamelContext()
+Exchange exchange = new DefaultExchange(context)
 
 
-    exchange.getIn().setBody(body)
+exchange.getIn().setBody(body)
+
+Message message = new Message(exchange)
+Map properties = ["propertyExample1":"value1", "propertyExample2":"value2"]
+Map headers = ["Content-Type":"application/xml", "customHeader":"custom value"]
+
+message.setHeaders(headers)
+message.setProperties(properties)
+message = processData(message)
+
+println(message)
 
 
-    message = new Message(exchange)
-    Map properties = ["propertyExample1":"value1", "propertyExample2":"value2"]
-    Map headers = ["Content-Type":"application/xml", "customHeader":"custom value"]
-
-    message.setHeaders(headers)
-    message.setProperties(properties)
-    def messageOut = processData(message)
-
-    println(messageOut)
-}
 //-----END Main class ( remember to not use it on CPI ) --------
 //********************************************************************
 
@@ -55,8 +51,9 @@ static void main(String[] args) {
     def breader =  msg.getBody(Reader)
      def root = new XmlSlurper().parse(breader)
 
-    root."**".findAll{it.name() == "ZTANKER_HEADER"}.each{
-        println("${it}")
+    root.CHILDCOMPLEX.TABLEDATA.each{
+        println(" TextField = ${it.FIELD.text()} / NumField = ${it.NUM.text()}")
+
     }
 
   return msg
